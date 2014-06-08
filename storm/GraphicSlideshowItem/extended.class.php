d82e7f97e75e94d8531ef65fc501d5bb03ce2a54
Gerardo <gerardooscarjt@gmail.com>
2014-06-08 02:28:31 +0200

    Fix GraphicSlideShow

--- storm/GraphicSlideshowItem/extended.class.php
+++ storm/GraphicSlideshowItem/extended.class.php
@@ -7,4 +7,17 @@
 
 	class GraphicSlideshowItem extends GraphicSlideshowItem_auto {
 
+		public static function INSERT() {
+			$row = parent::INSERT();
+
+			$row->setBackground(Image::INSERT('resources/default-image.png'));
+			$row->setTitle(Label::INSERT());
+			$row->setText(SimpleText::INSERT());
+			$row->setEnabled(1);
+
+			return $row;
+		}
+
+
+
 	}
