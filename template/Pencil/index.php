1b044664cebd05e79384a31978690749dec89094
Gerardo <gerardooscarjt@gmail.com>
2014-07-01 22:49:46 +0200

    try to fix template Pencil

--- template/Pencil/index.php
+++ template/Pencil/index.php
@@ -1,13 +1,13 @@
 <div id="frame">
 	<header class="main">
 		<div id="menu-idiomas">[[COMPONENT name=MenuLanguages]]</div>
-		<div class="logo">[[COMPONENT name=Image id=1 style=height:60]]</div>
-		<h1><a href="/">[[COMPONENT name=Label text=Título id=6]]</a></h1>
-		<h2>[[COMPONENT name=Label text=Subtítulo id=7]]</h2>
+		<div class="logo">[[COMPONENT name=Image style=height:60 id=4]]</div>
+		<h1><a href="/">[[COMPONENT name=Label text=Título id=14]]</a></h1>
+		<h2>[[COMPONENT name=Label text=Subtítulo id=15]]</h2>
 	</header>
 	<div id="body">
 		<div id="marco_migas">
-			[ [COMPONENT name=Breadcrumb]]		
+			[[COMPONENT name=Breadcrumb]]		
 		</div>
 		<div id="right">
 			[[COMPONENT name=Menu]]
