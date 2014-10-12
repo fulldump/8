5f60ffbbb541dd6be7907aea6f33acf1339ce8c5
Gerardo <gerardooscarjt@gmail.com>
2014-10-12 20:40:25 +0200

    Fix TrunkToolbar

--- component/TrunkToolbar/index.php
+++ component/TrunkToolbar/index.php
@@ -1,23 +1,23 @@
 <div component="TrunkToolbar">
 	<div class="toolbar">
 		<div class="expand-button link icon" onclick="this.parentNode.parentNode.classList.toggle('expanded');"></div>
-		<a href="/Adminx/" class="logo link icon">TreeWeb</a>
+		<a href="<?=Router::getNodeUrl(Router::$root->get('adminx'))?>" class="logo link icon">TreeWeb</a>
 		<div class="right">
 			<a href="/Profile" class="link icon user"><span class="user-name"><?php echo Session::getUser()->getName(); ?></span></a><a href="#" onclick="document.getElementById('form-logout').submit()" class="logout link icon"></a>
 		</div>
 		<div class="left">
-			<?php // Left menu
+			<?php 
+				$adminx = Router::$root->get('adminx');
+				foreach ($adminx->children as $child) {
+					if ($child->id == Router::$node->id) {
+						$selected = ' selected';
+					} else {
+						$selected = '';
+					}
 
-			
-			$adminx = Router::$root->get('adminx');
-			foreach ($adminx->children as $child) {
-				if ($child->id == Router::$node->id) {
-					$selected = ' selected';
-				} else {
-					$selected = '';
+					echo '<a class="link'.$selected.'" href="'.Router::getNodeUrl($child).
+						'">'.$child->getProperty('title').'</a>';
 				}
-			?><a href="<?=Router::getNodeUrl($child)?>" class="link<?=$selected?>"><?=$child->getProperty('title');?></a><?php
-			}
 			?>
 		</div>
 	</div>
