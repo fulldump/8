a7f654d07f7317065c6e4932cce767610fa5fb75
gerardo <gerardooscarjt@gmail.com>
2014-10-25 19:54:25 +0200

    Start three components: AdminxNodePage, AdminxNodeProperties, AdminxNodeRoot

---
+++ component/AdminxNodePage/index.js
@@ -0,0 +1,20 @@
+// Component AdminxNodePage
+
+
+(function(){
+	'use strict';
+
+	
+	function AdminxNodePage() {
+		
+		this.dom = document.createElement('div');
+		this.dom.setAttribute('component', 'AdminxNodePage');
+
+		this.dom.innerHTML = 'I am AdminxNodePage';
+		
+	}
+	
+
+	window.AdminxNodePage = AdminxNodePage;
+	
+})();
