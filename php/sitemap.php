ca64ce1b3f8df0bf7217c376bb99bb4d13eca46d
gerardo <gerardooscarjt@gmail.com>
2015-03-12 22:50:47 +0100

    Improvements in php/sitemap.php

--- php/sitemap.php
+++ php/sitemap.php
@@ -1,39 +1,39 @@
 <?php
-	header('Content-Type: text/xml; charset=UTF-8');
-	
-	echo '<?xml version="1.0" encoding="UTF-8"?>'."\n";
-	
+header('Content-Type: text/xml; charset=UTF-8');
+
+echo '<?xml version="1.0" encoding="UTF-8"?>'."\n";
+
 ?>
 <urlset xmlns="http://www.google.com/schemas/sitemap/0.84">
-<?php
+	<?php
 
-	function type($node) {
-		return $node->getProperty('type');
+function type($node) {
+	return $node->getProperty('type');
+}
+
+function path($node) {
+
+}
+
+function traverse($node) {
+	draw($node);
+	foreach ($node->children as $child) {
+		traverse($child);
+	}
+}
+
+function draw($node) {
+	if (null === $node->parent) {
+		return;
 	}
 
-	function path($node) {
-
+	if (false === $node->getProperty('sitemap')) {
+		return;
 	}
 
-	function traverse($node) {
-		draw($node);
-		foreach ($node->children as $child) {
-			traverse($child);
-		}
-	}
-
-	function draw($node) {
-		if (null === $node->parent) {
-			return;
-		}
-
-		if (false === $node->getProperty('sitemap')) {
-			return;
-		}
-
-		$url = "http://{$_SERVER['HTTP_HOST']}".Router::getNodeUrl($node);
-		$date = date('Y-m-d', time());
-		echo <<<heredoc
+	$url = "http://{$_SERVER['HTTP_HOST']}".Router::getNodeUrl($node);
+	$date = date('Y-m-d', time());
+	echo <<<heredoc
 	<url>
 		<loc>$url</loc>
 		<lastmod>$date</lastmod>
@@ -42,9 +42,9 @@
 	</url>
 
 heredoc;
-	}
+}
 
-	Router::load();
-	traverse(Router::$root);
-?>
+Router::load();
+traverse(Router::$root);
+	?>
 </urlset>
